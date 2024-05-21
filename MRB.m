b535881eb2b22ca7edf3310512886a5cff3ed13e
Lpopvil <113474413+Lpopvil@users.noreply.github.com>
2024-05-21 16:04:45 +0200

    Add files via upload

---
+++ MRB.m
@@ -0,0 +1,15 @@
+m =4;
+l =1;
+
+%If the motors are 1m distance from the center we have:
+%x_i=y_i=l, z_i=0 
+%sum[4__i=1] = 0^2+l^2+0^2+(-l)^2
+Ixx = l^2 + (-l)^2;
+Iyy = Ixx;
+Izz = l^2 + (-l)^2 + l^2 + (-l)^2;
+
+Ic = [Ixx,0,0;0,Iyy,0;0,0,Izz];
+
+Mrb =[m*eye(3),zeros(3);zeros(3),Ic];
+
+g = 9.81;
