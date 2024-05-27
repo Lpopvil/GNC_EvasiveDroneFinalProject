b34f2cc63362a8df7ec653da5b2fe39eb96aa94f
Lpopvil <leiapopper@gmail.com>
2024-05-27 14:47:01 +0200

    Polinomial function to make the quadcopter rotate

--- MRB.m
+++ MRB.m
@@ -14,3 +14,27 @@ Mrb =[m*eye(3),zeros(3);zeros(3),Ic];
 test =0; 
 g = 9.81;
 %hello does it work?
+
+
+% motion plannning for theta angle from 0 to 360 degrees in 10 seconds
+% Jerome Jouffroy, January 2024
+
+
+%------------------
+% motion planning.
+%------------------
+T = 5; % horizon time
+% intial conditions
+theta0 = 0;
+theta_dot0 = 0;
+theta_ddot0 = 0;
+
+% final conditions
+thetaT = 45;
+theta_dotT = 0;
+theta_ddotT = 0;
+
+% calculations polynomial coefficients
+alpha_vect = poly5traj([theta0;theta_dot0;theta_ddot0],[thetaT;theta_dotT;theta_ddotT],T);
+%alpha_vect = poly3traj([theta0; theta_dot0], [thetaT; theta_dotT], T);
+
